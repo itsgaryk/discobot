c21265e42bde7a4b44c7659438c523a7796eeda1
itsgaryk <gary@kimmelman.co.uk>
2021-03-02 16:12:43 +0000

    getting there. Add custom search with -radio

--- functions.js
+++ functions.js
@@ -2,36 +2,24 @@ const icy = require("icy")
 const fetch = require("node-fetch");
 const AbortController = require("abort-controller")
 
-const player = require("./player.js")
+const stationDirectory = require("./stations-directory.json");
+const stationSaved = require("./stations-saved.json");
+const randomStationList = [];
 
-const testMe = "hello"
-let stream;
 let logchannel;
 let icyReader;
+let autoNowPlaying;
 let radioStation;
 let nowPlaying;
 
-const stationDirectory = require("./stations-database.json");
-const stationDirectoryLength = stationDirectory.length;
-const stationCustomDirectory = require("./stations-saved.json");
-
-//only used internall by stationDirectoryCountries
-function getCountries(){
-	const newList = [];
-	for(let i = 0; i < stationDirectoryLength; i++){
-		if(!newList.some(item => item === stationDirectory[i].country))
-			newList.push(stationDirectory[i].country);
-	}
-	return newList;
-}
-
-const stationDirectoryCountries = getCountries();
+let streamConnection;
+let streamDispatcher;
 
 module.exports = {
 	configLogChannel,
 	commandRadio,
-	commandCountries,
-	commandStop
+	commandStop,
+	commandAuto
 }
 
 //Used in client.once("ready") to get the Discord channel object
@@ -39,30 +27,50 @@ function configLogChannel(channel){
 	logchannel = channel
 }
 
-function commandRadio(voice, args){
-	//Returns a random radio station from a submitted list
-	const getRandomStation = (list) => {
+//Returns a random radio station from a submitted list
+function getRandomStation(list){
 		return list[Math.floor(Math.random() * Math.floor(list.length))];
 	}
 
-	//0 Arguments - Plays a random radio station from the database
+function commandAuto(){
+	if(icyReader !== undefined){
+		if(icyReader.isPaused())
+		{
+			autoNowPlaying === true;
+			icyReader.resume();
+			clientLogMessage("Auto \"Now Playing\" is turned on");
+		}
+		else
+		{
+			autoNowPlaying === false;
+			icyReader.pause();
+			clientLogMessage("Auto \"Now Playing\" is turned off");
+		}
+	}
+	else
+		clientLogMessage("Radio is currently not playing");
+}
+
+function commandRadio(voice, args){
+	//0 arguments
+	//Plays a random radio station from the database
 	if(args[0] === undefined){
 		playRadio(voice, getRandomStation(stationDirectory), "random");
 		return;
 	}
 
-	//1 Argument only
-	if(args[1] === undefined){
+	//1 argument only
+	if(args[1] === undefined){	
 		//Checks if the argument is a number (for saved stations)
 		try{
 			const checkNumber = Number.parseInt(args[0]);
-			for(let i = 0; i < stationCustomDirectory.length; i++){
+			for(let i = 0; i < stationSaved.length; i++){
 				if (checkNumber === i+1){
-					playRadio(voice, stationCustomDirectory[i])
+					playRadio(voice, stationSaved[i]);
 					return;
 				}
 			}
-		} catch (e){console.log(e)}
+		} catch(e){}
 
 		//Checks if the argument is a URL
 		try{
@@ -72,152 +80,166 @@ function commandRadio(voice, args){
 		} catch(e){}
 	}
 
-	//Checks if the arguments are a country - a country can be more than 1 word
-	const country = args.join(" ");
-	
-	for(let i = 0; i < stationDirectoryCountries.length; i++){
-		if(stationDirectoryCountries[i].toLowerCase() === country){
-			const newList = [];
-			for(const item of stationDirectory){
-				if(item.country.toLowerCase() === country) newList.push(item);
+	//1 or more arguments 
+	//Smart search. Checks all properties in each Object
+	for(const item of stationDirectory){
+		for(const property in item){
+			if(item[property].toLowerCase() === args.join(" ").toLowerCase()){
+				randomStationList.push(item);
 			}
-			playRadio(voice, getRandomStation(newList), "random");
+		}
+	}
+	if(randomStationList.length === 1){
+		clientLogMessage(`Found 1 result for **${args.join(" ")}**`);
+		playRadio(voice, randomStationList.shift());
+		return;
+	}
+	if(randomStationList.length > 1){
+		clientLogMessage(`Found ${randomStationList.length} results for **${args.join(" ")}**`);
+		playRadio(voice, getRandomStation(randomStationList), "filter");
+		return;
+	}
+
+	//2 or more arguments
+	//Search by field/property name
+	const attributes = Object.keys(stationDirectory[0])
+	const attribute = args.shift()
+	if(attributes.some(i => i === attribute)){
+		for(const item of stationDirectory){
+			for(const property in item){
+				if(property === attribute)
+					if(item[property].toLowerCase() === args.join(" ").toLowerCase())
+						randomStationList.push(item);
+			}
+		}
+		if(randomStationList.length === 1){
+			clientLogMessage(`Found 1 result for **${attribute}** - \`${args.join(" ")}\``,);
+			playRadio(voice, randomStationList.shift())
+			return;
+		}
+		if(randomStationList.length > 1){
+			clientLogMessage(`Found ${randomStationList.length} result for **${attribute}** - \`${args.join(" ")}\``,);
+			playRadio(voice, getRandomStation(randomStationList), "filter")
 			return;
 		}
 	}
-/*
-	//Checks if the first argument is one of the radio properties
-	const attributes = Object.keys(radios[0])
-	const attribute = args.shift()
-	//Iterating stations in the database
-	const newList = radios.map(i => {
-		//Iterating attributes
-		for(let j = 0; j < attributes.length; j++){
-			//Checks for the property
-			if(i[attributes[j]] !== undefined){
-				//Checks if the property has the value of the remaining arguments
-				if(i[attributes[j]] === args.join(" ")){
-					return i;
-				}
-			}
-		}
-	})
-
-	//Checks if the iterator found any stations matching the user's criteria
-	if(newList.length > 0){
-		playStation(voice, randomStation(newList));
-		return;
-	} else {
-		clientLogMessage(`Unable to find a station based on your search criteria`);
-		return;
-	}
-*/
-}
-
-function commandCountries(){
-	clientLogMessage("Countires for the radio search\n" + stationDirectoryCountries.join("\t"));
+	//If unsuccessful
+	clientLogMessage(`Unable to find a station based on your search criteria`);
 }
 
 function commandStop(){
-	if(icyReader !== undefined) icyReader.removeAllListeners();
-
-	stream.disaptcher.destroy();
-	clientLogMessage("Playback is stopped");
+	if(streamDispatcher !== undefined){
+		streamDispatcher.destroy();
+		if(icyReader !== undefined) icyReader.removeAllListeners();
+		if(streamConnection !== undefined) streamConnection.removeAllListeners();
+		//clientLogMessage("Playback is stopped")
+	}
+	else{
+		clientLogMessage("Nothing is currently playing");
+	}
 }
 
-function playRadio(voice, station, random){
+function playRadio(voice, station, type){
 	const getICY = () => {
 		icy.get(station.url, i => {
 			icyReader = i;
-			
-			i.on('metadata', metadata => {
-					let icyData = icy.parse(metadata);
-					if(icyData?.StreamTitle){
-						if(nowPlaying !== icyData.StreamTitle){
-							nowPlaying = icyData.StreamTitle;
-							clientLogMessage("Now playing: " + icyData.StreamTitle)
-						}
-					}
-					else
-						streamPlaying = `Unable to find track info`;
-			})
-	
-			i.on("error", error => {
-				console.log(error.code)
-				clientLogMessage("Unable to retrieve song info");
-			})
 
+			i.on('metadata', metadata => {
+				const genericMessages = [
+					"Use HTTP to feed the song name",
+					"-",
+					"Live",
+					"",
+					null,
+					undefined
+				]
+				let icyData = icy.parse(metadata);
+				
+				if(icyData?.StreamTitle !== null){
+					if((genericMessages.some(m => m === icyData.StreamTitle) || icyData.StreamTitle.length < 3) && nowPlaying !== `Unable to find track info`){
+							nowPlaying = `Unable to find track info`;
+							clientLogMessage(nowPlaying);
+					}else{
+						if(icyData.StreamTitle !== nowPlaying){
+							nowPlaying = icyData.StreamTitle;
+							clientLogMessage("Now playing: " + icyData.StreamTitle);
+						}
+					}							
+				}else{
+					nowPlaying = `Unable to find now playing info`;
+					clientLogMessage(nowPlaying);
+				}
+			})
+			
+			i.on("error", error => {
+				console.log(error.code + "Error when trying to retrieve metadata")
+				nowPlaying = `Unable to find track info`;
+				clientLogMessage(nowPlaying);
+				i.destroy();
+				icyData = undefined;
+			})
 			i.resume();
 		})
 	}
 
-	if (radioStation?.url === station.url){
+	if (radioStation?.url === station?.url){
 		clientLogMessage("Radio sation is already playing");
 		return;
 	}
 
-	console.log(`Checking radio station ${station.name} - ${station.url}`);
-	validateURL(station.url).then(e => {
-		if(icyReader !== undefined) icyReader.removeAllListeners();
-		if(stream !== undefined) stream.destroy();
+	console.log(`CHECKING: radio station ${station.name} - ${station.url}`);
+	validateURL(station.url).then(() => {
+		console.log("SUCCESS: validated stream URL")
 
-		//If only the URL is present (custom radio station)
-
-		//Populates property values with null if empty or non-existent
-		if(station?.name === null){
-			clientLogMessage("Connected to unknown radio station")
-			station.name = null;
-		}
-		//If all properties are present send the full message
-		if(station?.name !== null && station?.location !== null && station?.country !== null)
-			clientLogMessage(`**Connected to Radio Station:** \`${station?.name}\`\t **From:** \`${station?.location}, `
-		+ `${station?.country}\`\t **Website:** \`${station?.website}\``);
-		if(station?.location === null)
-			station.location = null;
-		if(station?.country === null)
-			station.country = null;
-
+		if(streamDispatcher !== undefined) commandStop();
 		radioStation = station;
-
-		//radioStation = station;
+		//If all properties are present send the full message
+		let counter = 0;
+		for(const property in radioStation){
+			if(radioStation[property] !== null && radioStation[property] !== "")
+				counter++;
+		}
+		if(counter === Object.keys(radioStation).length){
+			clientLogMessage(`**Connecting to Radio Station:** \`${radioStation?.name}\`\t **From:** \`${radioStation?.location}, `
+			+ `${radioStation?.country}\`\t **Website:** \`${radioStation?.website}\``);
+		}else{
+			clientLogMessage("Now Playing - " + radioStation.url);
+		}
 		getICY();
-		player(voice, stream, station.url, clientLogMessage);
+		playStream(voice)
+		randomStationList.splice(0, randomStationList.length);
+		//Disables auto Now Playing if false
+		if(autoNowPlaying === false)
+			icyReader.pause();
 	})
+	//The catch will trigger when the validateURL method fails the HTTP fetch request
 	.catch(error => {
-		console.log("Invalid URL or SHOUTCAST server is not responding");
-		if(random === "random") console.log("oui!");
+		console.log(error + "Invalid URL or SHOUTCAST server took too long to respond");
+		switch(type){
+			case "random":
+				playRadio(voice, getRandomStation(stationDirectory)); break;
+			case "filter":
+				if(randomStationList.length > 0) playRadio(voice, getRandomStation(randomStationList)); break;
+			default:
+				clientLogMessage("Failed to connect to radio station"); break;
+
+		}
 	});
 }
 
 async function validateURL(url){
 	const controller = new AbortController();
 	const signal  = controller.signal
-	const timeout = setTimeout(() => controller.abort(), 1000);
+	const timeout = setTimeout(() => controller.abort(), 1500);
 
 	await fetch(url, {signal: controller.signal})
-	clearTimeout(timeout);
 }
 
 function clientLogMessage(message) {
 	logchannel.send(message);
-	console.log(message);
+	//console.log(message);
 }
 /*
-function commandAuto(){
-	if(icyReader === undefined)
-		clientLogMessage("Radio is currently not playing");
-	else{
-		if(icyReader.isPaused()){
-			icyReader.resume();
-			clientLogMessage("Auto Display is turned on");
-		}
-		else{
-			icyReader.pause();
-			clientLogMessage("Auto Display is turned off");	
-		}
-	}
-}
-
 function getStationInfo(){
 	if(radioPlaying?.url === "" || radioPlaying?.name === "")
 		clientLogMessage("Currently not playing a radio station");
@@ -235,25 +257,83 @@ function getStationInfo(){
 			}, internetRadio.StreamSource.STREAM);
 }
 
-function getSavedStations(){
+function commandList(){
 	//theMessage.push("List of available radio stations.\n");
 	const stations = list.map(i => list.indexOf(i)+1 + ".\t" + i.name + "\n");
 	clientLogMessage("Favourited stations. Enter +radio [number] e.g. \`+radio 1\`\n\n" + stations.join(""));
 }
 
-function saveRadioStation(){
+function commandSave(){
 	list.push(radioStation);
 	fs.writeFileSync(JSON.stringify(list))
 }
-
-function stopStream(){
-	if(streamDispatcher !== ""){
-		streamDispatcher.destroy();
-		clearRadioPlaying();
-		icyReader.removeAllListeners();
-		clientLogMessage("Player has stopped");
-	}
-	else
-		clientLogMessage("Nothing is currently playing");
-}
 */
+
+function playStream(voice){
+    if(streamConnection === undefined)
+        voice.join()
+		.then(async connection => {
+			streamConnection = connection;
+			connection.on("debug", e => {
+				if (e.includes('[WS] >>') || e.includes('[WS] <<')) return;
+				console.log("Status: Connection warning - " + e);
+				if(e.includes('[WS] closed')) commandStop();
+			});
+			connection.on("disconnect", () => {
+				console.log("Bot disconnected from channel");
+				commandStop();
+			});
+			connection.on("error", e => {
+				clientLogMessage("Status: Error. See logs");
+				console.log(e);
+				commandStop();
+			});
+			connection.on("finish", () => {
+				console.log("Left the voice channel")
+			});
+			initDispatcher(connection);
+		})
+		.catch(error => {
+			console.log(error.code + "Error when trying to play");
+    	});
+    else
+        initDispatcher(streamConnection);
+}        
+
+function initDispatcher(connection) {
+    
+    streamDispatcher = connection.play(radioStation.url, {
+        volume: false,
+        highWaterMark: 512,
+        bitrate: 128,
+        fec: true
+    })
+
+    streamDispatcher.setBitrate(128);
+    streamDispatcher.setFEC(true);
+
+	streamDispatcher.once("speaking", () => {
+		console.log("SUCCESS: streaming URL")
+	})
+
+    streamDispatcher.on("finish", () => {
+		commandStop();
+		clientLogMessage("Status: Radio station has stopped broadcasting");
+		//if(type === "filter") playRadio();
+    });	
+
+	//On spams the error message like Failed to send a packet - no UDP socket
+    streamDispatcher.once("debug", e => {
+        clientLogMessage("Status: Dispatcher warning - " + e);
+		commandStop();
+    });
+
+    streamDispatcher.on("disconnect", () => {
+        clientLogMessage("Status: Connection disconnect");
+    });
+
+    streamDispatcher.on("error", e => {
+        clientLogMessage("Status: Broadcast connection error");
+        console.log(e);
+    });
+}
--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const Discord = require('discord.js');
 const functions = require("./functions.js")
 const client = new Discord.Client({
-    presence:{
-        activity: {
-            name: "+help for commands",
-            type: "LISTENING"
-        }
-    },
     ws : {
         intents: [
             'GUILDS', 
@@ -24,10 +18,18 @@ const {
 
 client.once('ready', () => {
 	console.log(`Locked and loaded!`);
+
     client.channels.fetch(logchannel)
     .then(channel => {
         functions.configLogChannel(channel);
         console.log("Log channel configured for functions.js");
+
+        client.user.setPresence({
+            activity: {
+                name: "commands in " + channel.name,
+                //type: "LISTENING"
+            }
+        })
     })
     .catch(error => console.log(error + "\tSomething went wrong"));
 });
@@ -35,28 +37,43 @@ client.once('ready', () => {
 client.on('message', async message => {
     const isInVoice = () => {
         //Checks if the member is in a voice channel
-        if(message.member?.voice?.channel === null)
-            {message.channel.send("You are currently not in a voice channel"); return false;}
-        //Checks if the member is in the same voice channel as the bot
-        if(message.guild?.voice?.channel?.id !== null && message.guild?.voice?.channel?.id !== message.member?.voice?.channel?.id)
-            {message.channel.send("You must be in the same channel as the bot to perform this command"); return false;}
-        return true;
+        if(message.member?.voice?.channel !== null){
+            //Checks if the bot is in a voice channel
+            if(message?.guild?.voice?.channelID === undefined)
+                return true;
+            else
+                //Checks if the member is in the same voice channel as the bot
+                 if (message.member?.voice?.channel.id === message?.guild?.voice?.channelID)
+                    return true;
+                else
+                    {message.channel.send("You must be in the same channel as the bot to perform this command"); return false;}
+        }else{
+            message.channel.send("You need to be in a voice channel in order to perform this command"); return false;
+        }
     }
+
 	//Ignores the message if sent from a bot
 	if(message.author.bot || message.channel.type === "dm" ) return;
 
     if (message.content.startsWith(prefix)){
+        if(message.channel.id !== logchannel)
+            return message.channel.send("Commands will only work in " + message.guild.channels.cache.get(logchannel));
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const command = args.shift();
 
         switch(command){
+            case("auto"):
+                functions.commandAuto();
+                break;
             case("radio"):
-                if(isInVoice) functions.commandRadio(message.member.voice.channel, args);
+                if(isInVoice()) functions.commandRadio(message.member.voice.channel, args);
                 break;
             case("list"):
                 //functions.commandList();
-            case("countries"):
-                functions.commandCountries();
+                break;
+            case("stop"):
+                if(isInVoice()) functions.commandStop();
+                break;
             default: (message.channel.send("Invalid command"))
         }
     }
